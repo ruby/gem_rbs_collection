667c889fcf35b5e5a7aed1350592fe7991d630e0
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-11-13 19:30:47 +0900

    Add retryable
    - https://rubygems.org/gems/retryable
    - https://github.com/nfedyashev/retryable

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-gem "rbs", git: "https://github.com/ruby/rbs.git", branch: "repo"
-gem "steep", git: "https://github.com/soutaro/steep.git", branch: "update-rbs"
+gem "rbs", git: "https://github.com/ruby/rbs.git", branch: "master"
+gem "steep", git: "https://github.com/soutaro/steep.git", branch: "master"
 
 gem "minitest"
---
+++ gems/retryable/3.0/_test/test.rb
@@ -0,0 +1,26 @@
+require "retryable"
+
+Retryable.retryable do
+  # ...
+end
+
+Retryable.configuration
+
+Retryable.configure do |config|
+  config.contexts = {}
+  config.ensure = ->(retries) { puts "retries: #{retries}" }
+  config.exception_cb = ->(exception) { puts exception.message }
+  config.log_method = ->(retries, exception) { puts "retries: #{retries}, exception=#{exception.message}" }
+  config.matching = /foo/
+  config.matching = "bar"
+  config.matching = [/foo/, "bar"]
+  config.not = ArgumentError
+  config.not = [ArgumentError]
+  config.on = ArgumentError
+  config.on = [ArgumentError]
+  config.sleep = 1
+  config.sleep = 0.5
+  config.sleep = ->(retries) { retries ** 2 }
+  config.sleep_method = ->(n) { sleep(n.to_int + 1.5) }
+  config.tries = 3
+end
