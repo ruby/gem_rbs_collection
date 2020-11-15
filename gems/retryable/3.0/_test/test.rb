0b1e92484aaf85ec54f4fb2b16fa76d7b8cf1b68
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-11-16 02:31:57 +0900

    Fix retryable RBS and test

--- gems/retryable/3.0/_test/test.rb
+++ gems/retryable/3.0/_test/test.rb
@@ -1,20 +1,61 @@
 require "retryable"
 
-Retryable.retryable do
+Retryable.retryable(tries: 0)
+
+str = Retryable.retryable do
+  "foo"
+end
+str&.ascii_only?
+
+Retryable.retryable do |retries, exception|
+  retries.bit_length
+  exception&.backtrace
+end
+
+Retryable.retryable(
+  ensure: ->(retries) { retries.bit_length },
+  exception_cb: ->(exception) { exception.backtrace },
+  log_method: ->(retries, exception) { [retries.bit_length, exception.backtrace] },
+  matching: [/foo/, "bar"],
+  not: [ArgumentError],
+  on: StandardError,
+  sleep: 10,
+  sleep_method: ->(n) { n ** 1 },
+  tries: 3
+) do
   puts "foo"
 end
 
-Retryable.retryable(tries: 3, on: ArgumentError) do
-  puts "foo"
+Retryable.configuration.tap do |config|
+  config.contexts.keys
+  config.ensure.call(1)
+  config.exception_cb.call(RuntimeError.new)
+  config.log_method.call(1, RuntimeError.new)
+  config.matching
+  config.not
+  config.on
+  config.sleep
+  config.tries.integer?
+  config.enabled
+  config.enabled?
+  config.enable
+  config.disable
+  config[:tries]
+  config.to_hash
+  config.merge({ sleep: 1 })
 end
 
-Retryable.configuration
+Retryable.configuration = Retryable.configuration
 
 Retryable.configure do |config|
-  config.contexts = {}
-  config.ensure = ->(retries) { puts "retries: #{retries}" }
-  config.exception_cb = ->(exception) { puts exception.message }
-  config.log_method = ->(retries, exception) { puts "retries: #{retries}, exception=#{exception.message}" }
+  config.contexts[:foo] = {
+    on: [ArgumentError],
+    sleep: 10,
+    tries: 5
+  }
+  config.ensure = ->(retries) { retries.bit_length }
+  config.exception_cb = ->(exception) { exception.backtrace }
+  config.log_method = ->(retries, exception) { [retries.bit_length, exception&.backtrace] }
   config.matching = /foo/
   config.matching = "bar"
   config.matching = [/foo/, "bar"]
@@ -28,3 +69,14 @@ Retryable.configure do |config|
   config.sleep_method = ->(n) { sleep(n.to_int + 1.5) }
   config.tries = 3
 end
+
+Retryable.with_context(:foo)
+Retryable.with_context(:foo, { sleep: 10 })
+str = Retryable.with_context(:foo, { sleep: 10, tries: 3 }) do |retries, exception|
+  retries.bit_length
+  exception&.backtrace
+  "foo"
+end
+str&.ascii_only?
+
+Retryable.retryable_with_context(:faulty_service)
