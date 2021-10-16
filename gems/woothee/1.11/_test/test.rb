0470d15bf5981e7bc1b7c45edbb8370126438291
Kenichi Takahashi <kenichi.taka@gmail.com>
2021-10-16 19:37:30 +0900

    add woothee types

--- gems/woothee/1.11/_test/test.rb
+++ gems/woothee/1.11/_test/test.rb
@@ -2,3 +2,14 @@
 # It is type checked by `steep check` command.
 
 require "woothee"
+
+result = Woothee.parse("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)")
+
+result[:name] # => "Internet Explorer"
+result[:category] # => :pc
+result[:os] # => "Windows 7"
+result[:os_version] # => "NT 6.1"
+result[:version] # => "8.0"
+result[:vendor] # => "Microsoft"
+
+Woothee.is_crawler("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)") # => false
