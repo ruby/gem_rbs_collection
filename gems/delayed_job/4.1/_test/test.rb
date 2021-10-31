43c7313702874ce3806d86ac9c835f4cefca640e
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2021-10-31 18:27:00 +0900

    Simplify test code for delayed_job gem
    The `test.rb` is almost empty so I merged test.rb and worker.rb

--- gems/delayed_job/4.1/_test/test.rb
+++ gems/delayed_job/4.1/_test/test.rb
@@ -1,4 +1,18 @@
-# Write Ruby code to test the RBS.
-# It is type checked by `steep check` command.
+Delayed::Worker.queue_attributes = {
+  high_priority: { priority: -10 },
+  low_priority: { priority: 10 }
+}
 
-require "delayed_job"
+Delayed::Worker.delay_jobs = ->(job) {
+  job.queue != 'inline'
+}
+
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.sleep_delay = 60
+Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.read_ahead = 10
+Delayed::Worker.default_queue_name = 'default'
+Delayed::Worker.delay_jobs = !Rails.env.test?
+Delayed::Worker.raise_signal_exceptions = :term
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
