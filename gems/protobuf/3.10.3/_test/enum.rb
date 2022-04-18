ed1b81a465813d0781d873afd00c8a03cd2f242b
Soutaro Matsumoto <matsumoto@soutaro.com>
2022-04-18 14:26:44 +0900

    Update test
    - Remove `.pb.rb` code
    - Update `steep_expectations.yml`

--- gems/protobuf/3.10.3/_test/enum.rb
+++ gems/protobuf/3.10.3/_test/enum.rb
@@ -1,10 +1,4 @@
 module Enum
-  class States < ::Protobuf::Enum
-    define :ALABAMA, 1
-    define :CALIFORNIA, 2
-    define :HOGE, 1
-  end
-
   States::ALABAMA
   States::ALABAMA.to_i
   States::ALABAMA.tag
