ecab2e53412979ec5e569bafcabdae78809c4826
Shojiro Yanagisawa <nipe0324@gmail.com>
2022-09-11 20:08:41 +0900

    Add test on holidays test.rb

--- gems/holidays/8.4/_test/test.rb
+++ gems/holidays/8.4/_test/test.rb
@@ -7,5 +7,12 @@ Holidays::WEEKS
 Holidays::MONTH_LENGTHS
 Holidays::DAY_SYMBOLS
 
-Holidays.on(Date.civil(2022, 8, 11), :jp)
-Holidays.between(Date.civil(2022, 8, 10), Date.civil(2022, 8, 12), :jp)
+holidays = Holidays.on(Date.civil(2022, 8, 11), :jp)
+holidays[0][:date]
+holidays[0][:name]
+holidays[0][:regions]
+
+holidays = Holidays.between(Date.civil(2022, 8, 10), Date.civil(2022, 8, 12), :jp)
+holidays[0][:date]
+holidays[0][:name]
+holidays[0][:regions]
