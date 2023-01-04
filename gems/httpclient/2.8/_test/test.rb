a60959edea439de474b0db995e3173372d940fe7
ksss <co000ri@gmail.com>
2023-01-04 23:01:56 +0900

    It is better to use interface
    since HTTPClient::DelegateMethods module does not exist

--- gems/httpclient/2.8/_test/test.rb
+++ gems/httpclient/2.8/_test/test.rb
@@ -1,6 +1,11 @@
 require "httpclient"
 
+HTTPClient.get("http://example.com").body
+
 client = HTTPClient.new
+if false
+  client.set_auth("http://example.com", "user", "pass")
+end
 res = client.get("http://example.com", query: {"q" => "v"})
 if res.status == 200
   puts res.body
