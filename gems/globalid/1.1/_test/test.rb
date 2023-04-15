3f10c89c997dfb12ddfa7c5372ead8f7f1633b21
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2023-04-15 18:01:15 +0900

    Add GlobalID RBS

--- gems/globalid/1.1/_test/test.rb
+++ gems/globalid/1.1/_test/test.rb
@@ -2,3 +2,20 @@
 # It is type checked by `steep check` command.
 
 require "globalid"
+
+GlobalID.app = 'blah'
+
+class Person
+  include GlobalID::Identification
+
+  attr_accessor :id
+
+  def self.find(id)
+    new.tap { |p| p.id = id }
+  end
+end
+
+person = Person.find(1)
+person_gid = person.to_global_id
+p person_gid.uri
+p person_gid.to_s
