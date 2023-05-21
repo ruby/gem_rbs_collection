f181af4d5940411b65c692e267201f315c38b855
m11o <m11o@hey.jp>
2023-05-21 21:14:57 +0900

    Add RBS signatures for ActiveRecord::Base.enum

--- gems/activerecord/6.0/_test/activerecord-generated.rb
+++ gems/activerecord/6.0/_test/activerecord-generated.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  enum status: { active: 0, inactive: 1 }, _suffix: true
 end
 
 user = User.new
--- gems/activerecord/7.0/_test/test.rb
+++ gems/activerecord/7.0/_test/test.rb
@@ -7,4 +7,10 @@ module Test
   class ApplicationRecord < ActiveRecord::Base
     primary_abstract_class
   end
+
+  class User < ApplicationRecord
+    enum status: { active: 0, inactive: 1 }, _suffix: true
+    enum :role, { admin: 0, user: 1 }, _prefix: true
+    enum :classify, %w[hoge fuga], _default: "hoge"
+  end
 end
