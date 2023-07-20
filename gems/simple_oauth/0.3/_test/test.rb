fd324df0112475a59623686050848829ba5d093c
sue445 <sue445@sue445.net>
2023-07-20 23:57:46 +0900

    Add test

--- gems/simple_oauth/0.3/_test/test.rb
+++ gems/simple_oauth/0.3/_test/test.rb
@@ -2,3 +2,14 @@
 # It is type checked by `steep check` command.
 
 require "simple_oauth"
+
+params = { status: "Test." }
+
+oauth_params = {
+  consumer_key: "consumer_key",
+  consumer_secret: "consumer_secret",
+  token: "access_token",
+  token_secret: "access_token_secret",
+}
+
+auth_header = SimpleOAuth::Header.new(:get, "https://api.twitter.com/1.1/statuses/update.json", params, oauth_params).to_s
