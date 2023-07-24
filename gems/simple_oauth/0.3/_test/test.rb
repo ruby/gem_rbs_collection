aeede7258d020bfd509541e0c8075ce833292409
Go Sueyoshi <sue445@users.noreply.github.com>
2023-07-24 11:08:01 +0900

    Add simple_oauth rbs (#359)
    * ./bin/init_new_gem simple_oauth
    
    * Write rbs
    
    * Add test
    
    * Remove comment
    
    ---------
    
    Co-authored-by: Masataka Pocke Kuwabara <kuwabara@pocke.me>

---
+++ gems/simple_oauth/0.3/_test/test.rb
@@ -0,0 +1,12 @@
+require "simple_oauth"
+
+params = { status: "Test." }
+
+oauth_params = {
+  consumer_key: "consumer_key",
+  consumer_secret: "consumer_secret",
+  token: "access_token",
+  token_secret: "access_token_secret",
+}
+
+auth_header = SimpleOAuth::Header.new(:get, "https://api.twitter.com/1.1/statuses/update.json", params, oauth_params).to_s
