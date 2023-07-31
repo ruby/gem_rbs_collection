ad73a10bcab5ba42ae76cbc57d603bdd708a8e71
Soutaro Matsumoto <matsumoto@soutaro.com>
2023-07-31 17:48:01 +0900

    Add test

--- gems/connection_pool/2.4/_test/test.rb
+++ gems/connection_pool/2.4/_test/test.rb
@@ -2,3 +2,9 @@
 # It is type checked by `steep check` command.
 
 require "connection_pool"
+
+pool = ConnectionPool.new(size: 5, timeout: 5) { Array.new }
+
+pool.with do |array|
+	array.push(123)
+end
