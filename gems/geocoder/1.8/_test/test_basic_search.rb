51f13547d36ce8bc12cb6f82e066b97753d280f6
greendrop <greendrop.devel@gmail.com>
2023-08-05 12:57:37 +0900

    Update geocoder rbs

--- gems/geocoder/1.8/_test/test_basic_search.rb
+++ gems/geocoder/1.8/_test/test_basic_search.rb
@@ -3,11 +3,11 @@
 require "geocoder"
 
 results = Geocoder.search("Paris")
-results.first.coordinates
+results.first&.coordinates
 
 results = Geocoder.search([48.856614, 2.3522219])
-results.first.address
+results.first&.address
 
 results = Geocoder.search("172.56.21.89")
-results.first.coordinates
-results.first.country
+results.first&.coordinates
+results.first&.country
