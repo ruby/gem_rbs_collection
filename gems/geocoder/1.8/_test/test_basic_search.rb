defa86b3be304030b6fe244615f77fb0c01ba0ec
Katsuyuki Sadamoto <greendrop.devel@gmail.com>
2023-08-07 14:54:46 +0900

    Add rbs for geocoder gem (#379)
    * bin/init_new_gem geocoder 1.8
    
    * Add test for geocoder
    
    * bundle exec rbs prototype rb gems/geocoder/1.8/_src/lib/geocoder.rb > gems/geocoder/1.8/geocoder.rbs
    
    * bundle exec rbs prototype rb gems/geocoder/1.8/_src/lib/geocoder.rb > gems/geocoder/1.8/geocoder.rbs
    
    * bundle exec rbs prototype rb gems/geocoder/1.8/_src/lib/geocoder/results/base.rb > gems/geocoder/1.8/geocoder/results/base.rbs
    
    * bundle exec rbs prototype rb gems/geocoder/1.8/_src/lib/geocoder/query.rb > gems/geocoder/1.8/geocoder/query.rbs
    
    * Update geocoder rbs
    
    * Remove geocoder manifest.yaml
    
    ---------
    
    Co-authored-by: Masataka Pocke Kuwabara <kuwabara@pocke.me>

---
+++ gems/geocoder/1.8/_test/test_basic_search.rb
@@ -0,0 +1,13 @@
+#Taken from https://github.com/alexreisner/geocoder/tree/v1.8.0
+
+require "geocoder"
+
+results = Geocoder.search("Paris")
+results.first&.coordinates
+
+results = Geocoder.search([48.856614, 2.3522219])
+results.first&.address
+
+results = Geocoder.search("172.56.21.89")
+results.first&.coordinates
+results.first&.country
