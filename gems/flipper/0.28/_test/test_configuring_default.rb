e0c44af86a04a5e6b57dd9a4f01e115efb62bde0
Katsuyuki Sadamoto <greendrop.devel@gmail.com>
2023-08-10 11:03:02 +0900

    Add rbs for flipper gem (#382)
    * bin/init_new_gem flipper 0.28
    
    * Add test for flipper
    
    * bundle exec rbs prototype rb gems/flipper/0.28/_src/lib/flipper.rb > gems/flipper/0.28/flipper.rbs
    
    * bundle exec rbs prototype rb gems/flipper/0.28/_src/lib/flipper/adapters/memory.rb > gems/flipper/0.28/flipper/adapters/memory.rbs
    
    * bundle exec rbs prototype rb gems/flipper/0.28/_src/lib/flipper/adapter.rb > gems/flipper/0.28/flipper/adapter.rbs
    
    * bundle exec rbs prototype rb gems/flipper/0.28/_src/lib/flipper/actor.rb > gems/flipper/0.28/flipper/actor.rbs
    
    * bundle exec rbs prototype rb gems/flipper/0.28/_src/lib/flipper/dsl.rb > gems/flipper/0.28/flipper/dsl.rbs
    
    * bundle exec rbs prototype rb gems/flipper/0.28/_src/lib/flipper/configuration.rb > gems/flipper/0.28/flipper/configuration.rbs
    
    * Update flipper rbs

---
+++ gems/flipper/0.28/_test/test_configuring_default.rb
@@ -0,0 +1,21 @@
+# Taken from https://github.com/flippercloud/flipper/blob/v0.28.0/examples/configuring_default.rb
+
+require 'bundler/setup'
+require 'flipper'
+
+# sets up default adapter so Flipper works like Flipper::DSL
+Flipper.configure do |config|
+  config.adapter { Flipper::Adapters::Memory.new }
+end
+
+puts Flipper.enabled?(:search) # => false
+Flipper.enable(:search)
+puts Flipper.enabled?(:search) # => true
+Flipper.disable(:search)
+
+enabled_actor = Flipper::Actor.new("1")
+disabled_actor = Flipper::Actor.new("2")
+Flipper.enable_actor(:search, enabled_actor)
+
+puts Flipper.enabled?(:search, enabled_actor)
+puts Flipper.enabled?(:search, disabled_actor)
