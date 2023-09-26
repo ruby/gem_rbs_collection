9c9ce97b46d0114ba2b3e3ddaf3215056c76b1ec
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-09-26 19:46:12 +0900

    Add types for rest-client gem
    A simple HTTP and REST client for Ruby, inspired by the Sinatra's
    microframework style of specifying actions: get, put, post, delete.
    
    refs: https://github.com/rest-client/rest-client

---
+++ gems/rest-client/2.1/_test/test.rb
@@ -0,0 +1,35 @@
+# Write Ruby code to test the RBS.
+# It is type checked by `steep check` command.
+
+require "rest-client"
+
+RestClient.get 'http://example.com/resource'
+
+RestClient.get 'http://example.com/resource', {params: {id: 50, 'foo' => 'bar'}}
+
+RestClient.get 'https://user:password@example.com/private/resource', {accept: :json}
+
+RestClient.post 'http://example.com/resource', {param1: 'one', nested: {param2: 'two'}}
+
+RestClient.post "http://example.com/resource", {'x' => 1}.to_json, {content_type: :json, accept: :json}
+
+RestClient.delete 'http://example.com/resource'
+
+RestClient::Request.execute(method: :get, url: 'http://example.com/resource',
+                            timeout: 10)
+
+RestClient::Request.execute(method: :get, url: 'http://example.com/resource',
+                            ssl_ca_file: 'myca.pem',
+                            ssl_ciphers: 'AESGCM:!aNULL')
+
+RestClient.get('http://example.com/resource') { |response, request, result, &block|
+  case response.code
+  when 200
+    p "It worked !"
+    response
+  when 423
+    raise SomeCustomExceptionIfYouWant
+  else
+    response.return!(&block)
+  end
+}
