02bdda92092533656f2eb55628379590b39ca20f
m11o <m11o@hey.jp>
2023-09-29 00:57:08 +0900

    Add dotenv-rails rbs test

--- gems/dotenv-rails/2.8/_test/test.rb
+++ gems/dotenv-rails/2.8/_test/test.rb
@@ -1,4 +1,17 @@
 # Write Ruby code to test the RBS.
 # It is type checked by `steep check` command.
 
-require "dotenv-rails"
+require 'dotenv'
+
+Dotenv.load
+Dotenv.load('file1.env', 'file2.env')
+
+Dotenv.load!
+Dotenv.load!('file1.env', 'file2.env')
+
+Dotenv.require_keys('KEY1', 'KEY2')
+
+Dotenv.parse('file1.env', 'file2.env')['KEY1']
+
+Dotenv.overload('file1.env', 'file2.env')
+Dotenv.overload!('file1.env', 'file2.env')
