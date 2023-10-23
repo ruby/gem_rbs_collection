6023657842d77c7f340805181c57608e40839dc5
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-10-23 11:26:09 +0900

    Add check-manifest-yaml.rb (#419)
    * Add check-manifest-yaml.rb
    
    Add `check-manifest-yaml.rb` to check manifest.yaml file on each gems.
    It verifies
    
    * manifest.yaml contains a "dependencies" field
    * each dependency contains a "name" field
    * each dependency name points a stdlib
    
    refs: #267
    
    * railties: Run check-manifest-yaml.rb on testing

---
+++ bin/check-manifest-yaml.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'pathname'
+require 'rbs'
+require 'yaml'
+
+def green(str)
+  "\e[32m#{str}\e[0m"
+end
+
+def stdlib?(name)
+  (RBS::Repository::DEFAULT_STDLIB_ROOT / name / '0').exist?
+end
+
+manifest_file = Pathname('manifest.yaml')
+if manifest_file.exist?
+  manifest = YAML.safe_load(manifest_file.read)
+
+  unless manifest && manifest.key?('dependencies') && !manifest['dependencies'].empty?
+    puts <<~END
+      #{manifest_file} exists but does not contain dependencies.
+      Please add dependencies to #{manifest_file} or remove it.
+    END
+    exit 1
+  end
+
+  manifest['dependencies'].each do |dep|
+    unless dep.key?('name')
+      puts "#{dep} does not contain the name of dependency."
+      exit 1
+    end
+
+    unless stdlib?(dep['name'])
+      puts "#{dep['name']} is not a standard library."
+      exit 1
+    end
+  end
+
+  puts green "manifest.yaml is valid. 🐾"
+else
+  puts green "manifest.yaml is not exist skipped. 🐾"
+end
