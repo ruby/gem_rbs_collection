5d7bbb53f56bb31f89907514099e4a2174866fa9
rhiroe <ride.poke@gmail.com>
2024-04-09 11:33:52 +0900

    add: RBS signeture of ActiveRecord::Encryption::EncryptableRecord module
    Generated automatically by typeprof.
    
    The method definitions in the class_methods block of ActiveSupport::Concern have been rewritten to the ClassMethods module.

--- gems/activerecord/7.0/_test/test.rb
+++ gems/activerecord/7.0/_test/test.rb
@@ -13,5 +13,17 @@ module Test
     enum :role, { admin: 0, user: 1 }, _prefix: true
     enum :classify, %w[hoge fuga], _default: "hoge"
     enum :hoge, fuga: 0, piyo: 1
+
+    encrypts :secret, :key
+    encrypts :token, deterministic: true
+    encrypts :phrase, ignore_case: true
   end
+
+  User.deterministic_encrypted_attributes
+  User.source_attribute_from_preserved_attribute(:phrase)
+  user = User.new(secret: 'dummy', key: 'dummy', token: 'dummy', phrase: 'dummy')
+  user.encrypt
+  user.encrypted_attribute?(:secret)
+  user.decrypt
+  user.ciphertext_for(:token)
 end
