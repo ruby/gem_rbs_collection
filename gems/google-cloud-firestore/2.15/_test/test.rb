95de9b9edd81ddbf3d4c13fe3a4d97c40ea3dea7
sue445 <sue445@sue445.net>
2024-05-16 20:04:02 +0900

    Remove scaffold comments

--- gems/google-cloud-firestore/2.15/_test/test.rb
+++ gems/google-cloud-firestore/2.15/_test/test.rb
@@ -1,6 +1,3 @@
-# Write Ruby code to test the RBS.
-# It is type checked by `steep check` command.
-
 require "google-cloud-firestore"
 
 firestore = Google::Cloud::Firestore.new(project_id: "test-project")
