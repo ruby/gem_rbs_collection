326d6d0a0555344854aeaf458a31f2cbc8fd728a
sue445 <sue445@sue445.net>
2024-05-16 19:57:42 +0900

    Add test

--- gems/google-cloud-firestore/2.15/_test/test.rb
+++ gems/google-cloud-firestore/2.15/_test/test.rb
@@ -2,3 +2,11 @@
 # It is type checked by `steep check` command.
 
 require "google-cloud-firestore"
+
+firestore = Google::Cloud::Firestore.new(project_id: "test-project")
+
+firestore.doc("users/test-user").set({ user_name: "test", url: "https://example.com/" })
+firestore.doc("users/test-user").update({ user_name: "test", url: "https://example.com/new" })
+firestore.doc("users/test-user").get.data
+firestore.doc("users/test-user").get.exists?
+firestore.doc("users/test-user").delete
