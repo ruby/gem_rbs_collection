ca29256e88ca864aab4acc7d139c7fc962445316
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-06-02 21:19:48 +0900

    activerecord: if/unless proc params can take an instance as an argument (#578)
    Proc objects for if/unless params to the validation and hook methods can
    take an instance of the AR model as an argument.

--- gems/activerecord/6.0/_test/activerecord-generated.rb
+++ gems/activerecord/6.0/_test/activerecord-generated.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   enum role: { admin: 0, user: 1 }, _prefix: :user_role
 
   validates :name, presence: true, if: -> { something }
+  validates :age, presence: true, if: ->(user) { user.something }
 
   before_save -> (obj) { obj.something; self.something }
   around_save -> (obj, block) { block.call; obj.something }
