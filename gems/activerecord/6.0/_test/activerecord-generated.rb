b6455611d4da2a172cf57ef568004e5b2d3d3a05
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-06-11 16:25:21 +0000

    activerecord: CollectionProxy#build can take Hash-like object
    The current definition of `#build`, `#craete` and `#create!` methods of
    `ActiveRecord::Associations::CollectionProxy#build` take a Hash object.
    
    But they can also take Hash-like object.  Usually, Rails apps pass an
    instance of `ActionController::Parameters`.  The technique is well known
    as "Mass Assignment".
    
    Internally, they expects the object should have `#each_pair` method.
    Therefore, this changes the signature of these methods to take a
    Hash-like object instead of Hash.
    
    refs: https://github.com/rails/rails/blob/v7.0.0/activemodel/lib/active_model/attribute_assignment.rb#L29

--- gems/activerecord/6.0/_test/activerecord-generated.rb
+++ gems/activerecord/6.0/_test/activerecord-generated.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   enum status: { active: 0, inactive: 1 }, _suffix: true
   enum role: { admin: 0, user: 1 }, _prefix: :user_role
 
+  has_many :articles
+
   validates :name, presence: true, if: -> { something }
   validates :age, presence: true, if: ->(user) { user.something }
 
@@ -12,7 +14,12 @@ class User < ActiveRecord::Base
   end
 end
 
-_user = User.new
+class Article < ActiveRecord::Base
+end
+
+user = User.new
+user.articles.build(Hash.new)
+user.articles.build(ActionController::Parameters.new)
 
 User.eager_load(:address, friends: [:address, :followers])
 User.includes(:address, :friends).to_a
