1ed6b830ebaccc0bab8a0a27f5f63ec58e6655ed
rhiroe <ride.poke@gmail.com>
2024-07-28 02:08:42 +0000

    Fixed steep error points in ActiveRecord 6.1
    - Symlink of _test/metadata.yaml in 6.0 to 6.1
    - Annotated `User#articles` definition using `@dynamic` and removed _test/steep_expectations.yml
    
    These files are added in https://github.com/ruby/gem_rbs_collection/pull/598

--- gems/activerecord/6.0/_test/activerecord-generated.rb
+++ gems/activerecord/6.0/_test/activerecord-generated.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   enum status: { active: 0, inactive: 1 }, _suffix: true
   enum role: { admin: 0, user: 1 }, _prefix: :user_role
 
+  # @dynamic articles
   has_many :articles
 
   validates :name, presence: true, if: -> { something }
