c802ad82162131b83d1cefe70abad9a7b17fa00e
Teppei Shintani <teppest1215@gmail.com>
2024-09-04 00:16:31 +0900

    Add google-apis-bigquery_v2 gems rbs signatures
    and, some other gems rbs signatures.
    
    - google-apis-core
    - googleauth
    - signet

--- gems/googleauth/1.11/_test/test.rb
+++ gems/googleauth/1.11/_test/test.rb
@@ -1,3 +1,10 @@
 require "googleauth"
 
-creds = Google::Auth::Credentials.new
+class MyCredentials < Google::Auth::Credentials
+  # Set the default scope for these credentials
+  self.scope = "http://example.com/my_scope"
+end
+
+Google::Auth::ServiceAccountCredentials.make_creds(
+  json_key_io: StringIO.new(''), scope: []
+)
