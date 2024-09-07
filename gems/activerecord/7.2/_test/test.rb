f0332a6eb57ac8da505996c06981bbbced3b7313
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-09-07 12:57:02 +0900

    activerecord: Add activerecord-7.2

---
+++ gems/activerecord/7.2/_test/test.rb
@@ -0,0 +1,45 @@
+# Write Ruby code to test the RBS.
+# It is type checked by `steep check` command.
+
+require "activerecord"
+
+module Test
+  class ApplicationRecord < ActiveRecord::Base
+    primary_abstract_class
+  end
+
+  class User < ApplicationRecord
+    enum status: { active: 0, inactive: 1 }, _suffix: true
+    enum :role, { admin: 0, user: 1 }, _prefix: true
+    enum :classify, %w[hoge fuga], _default: "hoge"
+    enum :hoge, fuga: 0, piyo: 1
+
+    encrypts :secret, :key
+    encrypts :token, deterministic: true
+    encrypts :phrase, ignore_case: true
+
+    normalizes :email, with: -> email {
+      # @type var email: String
+      email.strip.downcase
+    }
+  end
+
+  User.where.missing.to_sql
+  User.deterministic_encrypted_attributes
+  User.source_attribute_from_preserved_attribute(:phrase)
+  User.insert({ id: 1, name: 'James' }, returning: %i[id name], unique_by: :id, record_timestamps: true)
+  User.insert!({ id: 1, name: 'James' }, returning: %i[id name], record_timestamps: true)
+  User.insert_all([{ id: 1, name: 'James' }], returning: %i[id name], unique_by: :id, record_timestamps: true)
+  User.insert_all!([{ id: 1, name: 'James' }], returning: %i[id name], record_timestamps: true)
+  User.upsert({ id: 1, name: 'James' }, returning: %i[id name], unique_by: :id, record_timestamps: true)
+  User.upsert_all([{ id: 1, name: 'James' }], returning: %i[id name], unique_by: :id, record_timestamps: true)
+  user = User.new(secret: 'dummy', key: 'dummy', token: 'dummy', phrase: 'dummy')
+  user.encrypt
+  user.encrypted_attribute?(:secret)
+  user.decrypt
+  user.ciphertext_for(:token)
+
+  user = User.new
+  user.normalize_attribute(:email)
+  User.normalize_value_for(:email, ' CRUISE-CONTROL@EXAMPLE.COM\n')
+end
