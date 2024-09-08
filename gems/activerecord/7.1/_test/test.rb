b3b6d323173bffc35e65821a25fd35e8fc058216
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-09-09 03:17:33 +0900

    activerecord: Support insert_all on relation
    ActiveRecord supports insert and upsert methods on relation classes
    (AssociationRelation and CollectionProxy) since v6.1.
    
    refs:
    
    * https://github.com/rails/rails/pull/38899
    * https://github.com/rails/rails/blob/v6.1.7.8/activerecord/CHANGELOG.md
    
    Note:
    
    The implementation will be moved into AR::Relation since v7.2.

--- gems/activerecord/7.1/_test/test.rb
+++ gems/activerecord/7.1/_test/test.rb
@@ -9,6 +9,9 @@ module Test
   end
 
   class User < ApplicationRecord
+    # @dynamic articles
+    has_many :articles
+
     enum status: { active: 0, inactive: 1 }, _suffix: true
     enum :role, { admin: 0, user: 1 }, _prefix: true
     enum :classify, %w[hoge fuga], _default: "hoge"
@@ -22,6 +25,9 @@ module Test
       # @type var email: String
       email.strip.downcase
     }
+  end
+
+  class Article < ActiveRecord::Base
   end
 
   User.where.missing.to_sql
@@ -38,6 +44,12 @@ module Test
   user.encrypted_attribute?(:secret)
   user.decrypt
   user.ciphertext_for(:token)
+  user.articles.insert({ id: 1, name: 'James' }, returning: %i[id name], unique_by: :id, record_timestamps: true)
+  user.articles.insert!({ id: 1, name: 'James' }, returning: %i[id name], record_timestamps: true)
+  user.articles.insert_all([{ id: 1, name: 'James' }], returning: %i[id name], unique_by: :id, record_timestamps: true)
+  user.articles.insert_all!([{ id: 1, name: 'James' }], returning: %i[id name], record_timestamps: true)
+  user.articles.upsert({ id: 1, name: 'James' }, returning: %i[id name], unique_by: :id, record_timestamps: true)
+  user.articles.upsert_all([{ id: 1, name: 'James' }], returning: %i[id name], unique_by: :id, record_timestamps: true)
 
   user = User.new
   user.normalize_attribute(:email)
