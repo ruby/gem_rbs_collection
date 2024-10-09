5d52d68b0acbbcb16f0dcbcaaea35a3389bfd407
Yutaka Kamei <kamei@yykamei.me>
2024-10-09 10:43:41 +0900

    Support Symbol value for :block in #allow_browser
    Thanks to https://github.com/rails/rails/pull/53158,
    the `:block` option now supports Symbol value.
    
    This patch follows the upstream changes.

--- gems/actionpack/7.2/_test/test.rb
+++ gems/actionpack/7.2/_test/test.rb
@@ -5,3 +5,7 @@ end
 class BarController < ActionController::Base
   allow_browser versions: { safari: 17.5, chrome: 127, ie: false }, block: -> { head :not_acceptable }
 end
+
+class CanalController < ActionController::Base
+  allow_browser versions: { safari: 17.5, chrome: 127, ie: false }, block: :handle_outdated_browser
+end
