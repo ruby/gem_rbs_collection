9b07b7bde4c9f656f3f6d785bd676320032cecb4
pvcresin <toriyama@sansan.com>
2024-11-06 22:01:57 +0900

    Add some methods to faker (#716)

--- gems/faker/2.23/_test/test.rb
+++ gems/faker/2.23/_test/test.rb
@@ -3,6 +3,11 @@
 
 require "faker"
 
+Faker::Address.street_name + 'string'
+Faker::Address.secondary_address + 'string'
+Faker::Address.building_number + 'string'
+Faker::Address.community + 'string'
+Faker::Address.mail_box + 'string'
 Faker::Name.name + 'string'
 Faker::Internet.email
 Faker::Name.unique == Faker::Name
@@ -12,6 +17,7 @@ Faker::Lorem.unique.exclude :string, [number: 6], %w[azerty wxcvbn]
 
 Faker::Config.random = Random.new(42)
 Faker::Company.bs + 'string'
+Faker::Company.name + 'string'
 Faker::Config.random = nil
 
 Faker::Config.locale = 'es'
