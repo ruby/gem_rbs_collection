7651e7b92c15bd5b4bc11fd3dd455be0ea571fd0
Go Sueyoshi <sue445@users.noreply.github.com>
2025-01-10 00:28:27 +0900

    rake: Support task arguments and `sh` with env hash (#765)
    * Support rake task arguments within task block
    
    * Support env hash to FileUtils#sh in rake

--- gems/rake/13.0/_test/test.rb
+++ gems/rake/13.0/_test/test.rb
@@ -10,5 +10,23 @@ class Task < Rake::TaskLib
     task :test do
       ruby "test/unittest.rb"
     end
+
+    task :test_with_1_arg, :name do |t, args|
+      value = args[:name]
+      puts "#{args[:name]}=#{value}"
+    end
+
+    task :test_with_2_args, %i[name1 name2] do |t, args|
+      args.each do |name, value|
+        puts "#{name}=#{value}"
+      end
+    end
+
+    task :test_with_sh do
+      sh "ruby test/unittest_without_env.rb"
+
+      env = { "RACK_ENV" => "test" }
+      sh env, "ruby test/unittest_with_env.rb"
+    end
   end
 end
