d539d7739a2b65e234057e46dce292dd353fc6cc
sue445 <sue445@sue445.net>
2025-01-09 23:30:26 +0900

    Support rake task arguments within task block

--- gems/rake/13.0/_test/test.rb
+++ gems/rake/13.0/_test/test.rb
@@ -10,5 +10,16 @@ class Task < Rake::TaskLib
     task :test do
       ruby "test/unittest.rb"
     end
+
+    task :test_with_1_arg, :name do |t, args|
+      value = args[:name]
+      puts "#{args[:name]}=#{value}"
+    end
+
+    task :test_with_2_args, %i[name1 name2] do |t, args|
+      args.each do |name, value|
+        puts "#{name}=#{value}"
+      end
+    end
   end
 end
