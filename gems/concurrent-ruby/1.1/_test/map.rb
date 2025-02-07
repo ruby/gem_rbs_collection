ecc800ce651c17ca89ba9918ebe7d10cf650c6c8
Jeremy Nicklas <jeremywnicklas@gmail.com>
2025-02-07 10:16:30 -0500

    Add Concurrent::Map#compute* methods (#785)

---
+++ gems/concurrent-ruby/1.1/_test/map.rb
@@ -0,0 +1,17 @@
+require 'concurrent-ruby'
+
+map = Concurrent::Map.new #: Concurrent::Map[Symbol, Integer]
+
+map[:a] = 1
+map.fetch(:b) { 2 }
+map.fetch(:c, 3)
+
+new_value = map.compute(:a) { |old_value| (old_value || 0) + 1 }
+new_value + 2
+map.compute(:a) { |_old_value| nil }
+
+new_value = map.compute_if_absent(:absent) { 3 }
+new_value + 2
+
+map.compute_if_present(:maybe_present) { |old_value| old_value + 1 }
+map.compute_if_present(:a) { |_old_value| nil }
