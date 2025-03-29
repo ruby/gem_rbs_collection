1b207abb31c92061e2ce58383503bb36294f7666
KITAITI Makoto <KitaitiMakoto@gmail.com>
2025-03-29 20:02:07 +0900

    csv: Add signature for CSV.open (#840)
    * Add signature for CSV.open
    
    * Add test for CSV.open
    
    * Update gems/csv/3.3/csv.rbs
    
    Co-authored-by: Yuki Kurihara <co000ri@gmail.com>
    
    * Add StriongIO to type of CSV.open's first argument
    
    * Remove unnecessary dependency from csv
    
    * Minimize tests for csv
    
    ---------
    
    Co-authored-by: Yuki Kurihara <co000ri@gmail.com>

--- gems/csv/3.3/_test/test.rb
+++ gems/csv/3.3/_test/test.rb
@@ -14,5 +14,17 @@ csv.headers
 [1, 2, 3].to_csv
 [1, 2, 3].to_csv(col_sep: '\t')
 
+CSV.open("example.csv") { |csv| csv.read }
+csv = CSV.open("example.csv")
+csv.read
+CSV.open("example.csv", invalid: :replace, undef: :replace, headers: :first_row) do |csv|
+  csv.read
+  csv.headers
+end
+
+CSV.open("example.csv", "w") { |csv| csv << ["a", "b", "c"] }
+csv = CSV.open("example.csv", "w")
+csv << ["a", "b", "c"]
+
 "1,2,3".parse_csv
 "1,2,3".parse_csv(col_sep: '\t')
