08af6a1cf0fdb337f0751f96d361a3056c9db69a
ksss <co000ri@gmail.com>
2025-04-06 16:33:39 +0900

    Fix testing for sidekiq

--- gems/sidekiq/6.2/_test/test_1.rb
+++ gems/sidekiq/6.2/_test/test_1.rb
@@ -2,10 +2,6 @@
 # Taken from: https://github.com/mperham/sidekiq/wiki/Getting-Started
 class HardWorker
   include Sidekiq::Worker
-
-  def perform(name, count)
-    puts "Performing #{name} #{count} times"
-  end
 end
 HardWorker.perform_async('bob', 5)
 HardWorker.perform_at(Time.now + 5*60, 'bob', 5)
--- gems/sidekiq/6.3/_test/test_1.rb
+++ gems/sidekiq/6.3/_test/test_1.rb
@@ -2,20 +2,12 @@
 # Taken from: https://github.com/mperham/sidekiq/wiki/Getting-Started
 class HardWorker
   include Sidekiq::Worker
-
-  def perform(name, count)
-    puts "Performing #{name} #{count} times"
-  end
 end
 HardWorker.perform_async('bob', 5)
 HardWorker.perform_at(Time.now + 5*60, 'bob', 5)
 
 class HardJob
   include Sidekiq::Job
-
-  def perform(name, count)
-    puts "Performing #{name} #{count} times"
-  end
 end
 HardJob.perform_async('bob', 5)
 HardJob.perform_at(Time.now + 5*60, 'bob', 5)
