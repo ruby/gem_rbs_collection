1e026936df951c269b64b7cba278132b0af42828
baba <45838693+babashunya@users.noreply.github.com>
2025-05-09 17:14:27 +0900

    Add belongs_to, has_one and has_many method on ActiveModelSerializers (#848)

--- gems/active_model_serializers/0.10/_test/test.rb
+++ gems/active_model_serializers/0.10/_test/test.rb
@@ -1,9 +1,19 @@
 require "active_model_serializers"
 
 module Test
+  class Author < ActiveModelSerializers::Model
+    # @dynamic name, name=, initialize
+    attributes :name
+  end
+
+  class Category < ActiveModelSerializers::Model
+    # @dynamic name, name=, initialize
+    attributes :name
+  end
+
   class Article < ActiveModelSerializers::Model
-    # @dynamic title, title=, body, body=, tags, tags=, initialize
-    attributes :title, :body, :tags
+    # @dynamic title, title=, body, body=, tags, tags=, author, author=, category, category=, initialize
+    attributes :title, :body, :tags, :author, :category
   end
 
   class Tag < ActiveModelSerializers::Model
@@ -21,9 +31,19 @@ module Test
     def tag_names
       object.tags.map(&:name)
     end
+
+    has_many :tags, serializer: TagSerializer
+    has_one :author
+    belongs_to :category
   end
 
-  resource = Article.new(title: "Hello", body: "World", tags: [Tag.new(name: "foo"), Tag.new(name: "bar")])
+  class TagSerializer < ActiveModel::Serializer
+    type :tag
+
+    attribute :name
+  end
+
+  resource = Article.new(title: "Hello", body: "World", tags: [Tag.new(name: "foo"), Tag.new(name: "bar")], author: Author.new(name: "John"), category: Category.new(name: "Tech"))
   ArticleSerializer.new(resource).as_json
 
   serialization = ActiveModelSerializers::SerializableResource.new(resource)
