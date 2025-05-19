1612b9f4e808c5beb2de51418f1e308b020e5c4e
Kaspar Vollenweider <vok@panter.ch>
2025-05-19 12:30:03 +0200

    feat(paranoia): add replacements for untyped

--- gems/paranoia/2.5/_test/test.rb
+++ gems/paranoia/2.5/_test/test.rb
@@ -3,11 +3,14 @@
 require "paranoia"
 
 class Product < ActiveRecord::Base
-  acts_as_paranoid
+  acts_as_paranoid delete_all_enabled: true
 
-  after_destroy      :update_document_in_search_engine
-  after_restore      :update_document_in_search_engine
-  after_real_destroy :remove_document_from_search_engine
+  scope :promoted, -> { where(promoted: true) }
+  scope :regular, -> { where(promoted: false) }
+
+  after_destroy :update_document_in_search_engine
+  after_restore :update_document_in_search_engine, if: :requires_search_engine_update?
+  after_real_destroy :remove_document_from_search_engine, if: ->(product) { product.deleted? }
 
   def update_document_in_search_engine
     # ...
@@ -16,15 +19,33 @@ class Product < ActiveRecord::Base
   def remove_document_from_search_engine
     # ...
   end
+
+  def requires_search_engine_update?
+    true
+  end
 end
 
 product = Product.new
+product.paranoid?
+product.paranoia_destroy
 
+product = Product.new
+product.paranoia_delete
+product.restore
+
+product = Product.new
+product.delete
+product.restore!
+
+product = Product.new
 product.destroy
-
+product.paranoia_destroyed?
+product.deleted?
 product.really_destroy!
 
-Product.with_deleted
-
-# TODO: Comment-in the following code after typing with_deleted
-# Product.with_deleted.where(id: 47)
+Product.with_deleted.regular
+Product.only_deleted.promoted
+Product.deleted.where(id: 47)
+Product.restore(47)
+Product.restore([47, 99])
+Product.paranoid?
