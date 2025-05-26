0947fa5c513e5087fa1f376f7d90dc5b2bb871b4
euglena1215 <teppest1215@gmail.com>
2025-05-26 14:58:23 +0900

    devise: Add support for authentication_keys
    - Add support devise method `authentication_keys` to allow
      customization of the keys used for authentication.

--- gems/devise/4.9/_test/test.rb
+++ gems/devise/4.9/_test/test.rb
@@ -7,5 +7,6 @@ end
 class User < ActiveRecord::Base
   devise :database_authenticatable, :omniauthable, :confirmable,
          :recoverable, :registerable, :rememberable,
-         :trackable, :timeoutable, :validatable, :lockable
+         :trackable, :timeoutable, :validatable, :lockable,
+         authentication_keys: [:email]
 end
