0562becab547550b96c3aea9fd94bc69ac7d45d8
yuzisee <yuzisee@users.noreply.github.com>
2025-06-04 12:00:48 -0700

    Okay, now try https://guides.rubyonrails.org/v7.0/active_record_validations.html#combining-validation-conditions again

--- gems/activerecord/6.0/_test/activerecord.rb
+++ gems/activerecord/6.0/_test/activerecord.rb
@@ -12,15 +12,14 @@ class TestCallbackObject < ActiveRecord::Base
   end
 
   # https://guides.rubyonrails.org/v6.0/active_record_validations.html#combining-validation-conditions
-  RAILS_60_DOCS_EXAMPLE = [:local_condition1, ->(my_rec) { my_rec.local_condition3 }]
+  RAILS_60_DOCS_EXAMPLE =
+    [:local_condition1, ->(my_rec) { my_rec.local_condition2 }, Proc.new { |my_rec| my_rec.local_condition3 }]
 
   validate :custom_validation, on: :update, unless: RAILS_60_DOCS_EXAMPLE
 
   def custom_validation
     if self.record_timestamps?
       self.errors.add(:base, 'testing only')
-      self.errors.add(:var1, 'test')
-      self.errors.add(:var2, 'test test')
     end
   end
 end
