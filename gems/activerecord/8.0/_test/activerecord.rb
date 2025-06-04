cf181b7c74c142089cd647a808c4322f8f69a3b9
yuzisee <yuzisee@users.noreply.github.com>
2025-06-04 11:18:06 -0700

    Cannot find the declaration of constant: `RAILS_60_DOCS_EXAMPLE`

--- gems/activerecord/8.0/_test/activerecord.rb
+++ gems/activerecord/8.0/_test/activerecord.rb
@@ -21,7 +21,7 @@ class TestCallbackObject < ActiveRecord::Base
   end
 
   # https://guides.rubyonrails.org/v6.0/active_record_validations.html#combining-validation-conditions
-  RAILS_60_DOCS_EXAMPLE = [:local_condition1, ->(my_rec) { my_rec.local_condition3 }] #: [Symbol, ^(TestCallbackObject) [self: TestCallbackObject] -> bool]
+  RAILS_60_DOCS_EXAMPLE = [:local_condition1, ->(my_rec) { my_rec.local_condition3 }]
 
   validate :custom_validation, on: :update, unless: RAILS_60_DOCS_EXAMPLE
 
