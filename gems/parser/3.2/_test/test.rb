05f25cf53209e661431018c0be30274ec22eb6f4
Koji NAKAMURA <kozy4324@gmail.com>
2025-06-09 23:38:33 +0900

    parser: Add types for Parser::Source

--- gems/parser/3.2/_test/test.rb
+++ gems/parser/3.2/_test/test.rb
@@ -2,9 +2,31 @@
 # It is type checked by `steep check` command.
 
 require "parser"
+require "parser/current"
 
 node = Parser::CurrentRuby.parse("2 + 2") or raise
 node.loc
 
 buffer = Parser::Source::Buffer.new(__FILE__)
 buffer.name
+
+node, comments = Parser::CurrentRuby.parse_with_comments(<<SOURCE)
+# comment
+2 + 2
+SOURCE
+node.location.node unless node.nil?
+comments.first.text
+range = comments.first.location.expression
+range.source_buffer
+range.begin_pos
+range.end_pos
+range.size
+range.length
+range.line
+range.first_line
+range.column
+range.last_line
+range.last_column
+range.column_range
+range.source_line
+range.source
