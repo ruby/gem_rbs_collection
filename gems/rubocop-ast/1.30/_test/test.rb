5664a00133ee8c24a3478eff5978150279b007e1
Koji NAKAMURA <kozy4324@gmail.com>
2025-06-12 09:21:38 +0900

    rubocop-ast: Add types for rubocop-ast
    Cover sample code from: https://docs.rubocop.org/rubocop-ast/

--- gems/rubocop-ast/1.30/_test/test.rb
+++ gems/rubocop-ast/1.30/_test/test.rb
@@ -2,3 +2,25 @@
 # It is type checked by `steep check` command.
 
 require "rubocop-ast"
+
+class MyRule < Parser::AST::Processor
+  include RuboCop::AST::Traversal
+
+  def on_sym(node)
+    puts "I found a symbol! #{node.value}"
+  end
+
+  def on_if(node)
+    puts "I found a if! #{node.if?}"
+  end
+end
+
+code = File.read(__FILE__)
+source = RuboCop::AST::ProcessedSource.new(code, 2.7)
+rule = MyRule.new
+ast = source.ast
+exit if ast.nil?
+ast.each_node { |n| rule.process(n) }
+ast.each_node(:sym) { |n| rule.process(n) }
+ast.each_node(:sym, :if) { |n| rule.process(n) }
+ast.each_node.each {}.each_node
