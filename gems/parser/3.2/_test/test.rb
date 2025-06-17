4ef0627c3332a39419dd53feb222a904a9e00cd0
Koji NAKAMURA <kozy4324@gmail.com>
2025-06-17 23:37:41 +0900

    parser: Add types for parser (#884)
    * Remove non-existent method definition in Parser::Source::Map
    
    * parser: Add types for Parser::Source::Map::Collection
    
    * parser: Add types for Parser::Source::Range
    
    * parser: Add types for Parser::Source::Map::Condition

--- gems/parser/3.2/_test/test.rb
+++ gems/parser/3.2/_test/test.rb
@@ -6,6 +6,13 @@ require "parser/current"
 
 node = Parser::CurrentRuby.parse("2 + 2") or raise
 node.loc
+node.loc.node
+node.loc.expression
+node.loc.line
+node.loc.first_line
+node.loc.last_line
+node.loc.column
+node.loc.last_column
 
 buffer = Parser::Source::Buffer.new(__FILE__)
 buffer.name
@@ -20,6 +27,8 @@ range = comments.first.location.expression
 range.source_buffer
 range.begin_pos
 range.end_pos
+range.begin
+range.end
 range.size
 range.length
 range.line
@@ -30,3 +39,30 @@ range.last_column
 range.column_range
 range.source_line
 range.source
+range.with
+range.with(begin_pos: 1, end_pos: 2)
+range.adjust
+range.adjust(begin_pos: 1, end_pos: 2)
+range.resize(1)
+range.join range
+range.intersect range
+range.disjoint? range
+range.overlaps? range
+range.contains? range
+range.contained? range
+range.crossing? range
+range.empty?
+
+node = Parser::CurrentRuby.parse("[1,2]") or raise
+if node.loc.is_a? Parser::Source::Map::Collection
+  node.loc.begin
+  node.loc.end
+end
+
+node = Parser::CurrentRuby.parse("if true then 1 else 2 end") or raise
+if node.loc.is_a? Parser::Source::Map::Condition
+  node.loc.keyword
+  node.loc.begin
+  node.loc.else
+  node.loc.end
+end
