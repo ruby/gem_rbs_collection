4bcb80f6f96c71125d6347a763a85e101b2e778e
ksss <co000ri@gmail.com>
2025-06-30 22:53:46 +0900

    Drop support v6.0 and v6.1

--- gems/activestorage/6.0/_test/test.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  has_one_attached :one_image
-  has_many_attached :many_image
-end
-
-user = User.new
-one = ActiveStorage::Attached::One.new("one_image", user)
-one.url
