86c91609fef2e5fa365b5cd3dcb9088ef41d63e6
rhiroe <ride.poke@gmail.com>
2025-07-03 21:06:12 +0900

    actioncontroller: fix render_to_string method signature
    Arguments can be passed, and the block is optional.
    
    actioncontroller: add render_to_string method signature to AbstractController::Rendering module

--- gems/actionpack/7.2/_test/test.rb
+++ gems/actionpack/7.2/_test/test.rb
@@ -5,3 +5,5 @@ end
 class BarController < ActionController::Base
   allow_browser versions: { safari: 17.5, chrome: 127, ie: false }, block: -> { head :not_acceptable }
 end
+
+ActionController::Base.new.render_to_string(template: 'foo', locals: { bar: 'baz' })
