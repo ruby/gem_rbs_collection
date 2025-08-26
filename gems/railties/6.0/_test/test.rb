b61403df7b7a5f2d9a5592a6a60fc03172aa95db
yuzisee <yuzisee@users.noreply.github.com>
2025-08-25 17:31:36 -0700

    1 method call with untyped receiver detected from test.rb. Please assign the expected type to the receiver

--- gems/railties/6.0/_test/test.rb
+++ gems/railties/6.0/_test/test.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :test1_models
 end
 
-raise <<~HEREDOC unless Rails.application.routes.url_helpers.polymorphic_path(:new_test1_model).instance_of?(String)
+raise <<~HEREDOC unless Rails.application.routes.url_helpers.polymorphic_path(:new_test1_model) == '/test1_models/new'
   But it should work, since:
   >>> Rails.application.routes.method(:url_helpers).source_location
   ["/usr/local/bundle/gems/actionpack-6.0.6.1/lib/action_dispatch/routing/route_set.rb", 478]
