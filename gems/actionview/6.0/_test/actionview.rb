d0e70c5f55e731d4f1d50c3ebc100bd72cf0c58e
Felipe Felix <56006792+felixefelip@users.noreply.github.com>
2025-10-28 14:23:33 -0300

    actionview: Fix form tags without blocks and some block types (#941)
    * actionview: Fix tags without blocks and block types
    
    * fix: indentation
    
    * move changes to gems/actionview/6.0/actionview.rbs
    
    * fix TrailingWhitespace
    
    * fix TrailingWhitespace

--- gems/actionview/6.0/_test/actionview.rb
+++ gems/actionview/6.0/_test/actionview.rb
@@ -11,4 +11,55 @@ class Test
       render "edit", status: :unprocessable_entity
     end
   end
+
+  class View
+    include ActionView::Helpers
+
+    def view_template_form
+      form_for @user do |f|
+        f.select :country, [ "USA", "Canada", "Mexico" ], include_blank: true
+        f.select :country, [ "USA", "Canada", "Mexico" ], include_blank: true do
+          "Custom Option"
+        end
+
+        f.collection_radio_buttons :country, [ "USA", "Canada", "Mexico" ], :to_s, :to_s
+        f.collection_radio_buttons :country, [ "USA", "Canada", "Mexico" ], :to_s, :to_s do |b|
+          b.label { b.radio_button }
+        end
+
+        f.collection_check_boxes :country, [ "USA", "Canada", "Mexico" ], :to_s, :to_s
+        f.collection_check_boxes :country, [ "USA", "Canada", "Mexico" ], :to_s, :to_s do |b|
+          b.label { b.check_box }
+        end
+
+        f.submit
+      end
+
+      select :country, [ "USA", "Canada", "Mexico" ], include_blank: true
+
+      select :country, [ "USA", "Canada", "Mexico" ], include_blank: true do
+        "Custom Option"
+      end
+
+      collection_check_boxes :user, :country, [ "USA", "Canada", "Mexico" ], :to_s, :to_s
+      collection_check_boxes :user, :country, [ "USA", "Canada", "Mexico" ], :to_s, :to_s do |b|
+        b.label { b.check_box }
+      end
+
+      collection_radio_buttons :user, :country, [ "USA", "Canada", "Mexico" ], :to_s, :to_s
+      collection_radio_buttons :user, :country, [ "USA", "Canada", "Mexico" ], :to_s, :to_s do |b|
+        b.label { b.radio_button }
+      end
+
+      label_tag :country, "Country Label"
+      label_tag :country, "Country Label" do
+        "Custom Label Content"
+      end
+
+      button_tag "Click Me"
+      button_tag "Click Me" do
+        "Custom Button Content"
+      end
+    end
+  end
 end
