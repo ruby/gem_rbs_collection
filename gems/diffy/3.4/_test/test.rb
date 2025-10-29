7b4a71ec20b5716e2c79a9d970f3de0a5bbc2d97
sue445 <sue445@sue445.net>
2025-10-29 09:18:00 +0900

    Add test

--- gems/diffy/3.4/_test/test.rb
+++ gems/diffy/3.4/_test/test.rb
@@ -2,3 +2,12 @@
 # It is type checked by `steep check` command.
 
 require "diffy"
+
+str1 = "123"
+str2 = "124"
+
+Diffy::Diff.new(str1, str2).to_s
+
+Diffy::Diff.new(str1, str2, context: 3).to_s
+
+Diffy::Diff.new(str1, str2).to_s(:html)
