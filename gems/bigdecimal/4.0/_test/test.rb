dda768fc7c02f4c4de70b2fbe885fd597ab0a6d0
ksss <co000ri@gmail.com>
2025-12-18 12:26:21 +0900

    Add BigDecimal v4

---
+++ gems/bigdecimal/4.0/_test/test.rb
@@ -0,0 +1,27 @@
+# Write Ruby code to test the RBS.
+# It is type checked by `steep check` command.
+
+require "bigdecimal"
+require "bigdecimal/util"
+
+BigDecimal("1.23")
+
+BigDecimal("1.23") + BigDecimal("1.23")
+BigDecimal("1.23") - BigDecimal("1.23")
+BigDecimal("1.23") * BigDecimal("1.23")
+BigDecimal("1.23") / BigDecimal("1.23")
+
+BigDecimal("1.23").to_d
+BigDecimal("1.23").to_f
+BigDecimal("1.23").to_i
+BigDecimal("1.23").to_r
+
+123.to_d
+12.3.to_d
+12r.to_d(3)
+0i.to_d
+
+require "bigdecimal/math"
+
+BigMath.E(10)
+BigMath.PI(10)
