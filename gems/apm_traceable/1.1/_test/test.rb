f02258f62f7f38ccfd18196bbc525bcdb7ccfbb8
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2025-12-31 12:26:59 +0900

    feat(apm_traceable): update type

--- gems/apm_traceable/1.1/_test/test.rb
+++ gems/apm_traceable/1.1/_test/test.rb
@@ -1,4 +1,22 @@
-# Write Ruby code to test the RBS.
-# It is type checked by `steep check` command.
+require 'apm_traceable'
 
-require "apm_traceable"
+ApmTraceable.configure do |config|
+  config.adapter = 'stdout'
+end
+
+class TracerTestClass
+  include ApmTraceable::Tracer
+  extend ApmTraceable::Tracer::ClassMethods
+
+  trace_methods :test_trace_method
+
+  def test_trace_method
+    'test'
+  end
+
+  def test_trace_span
+    trace_span('test_name', option1: :value1) do
+      'test'
+    end
+  end
+end
