51d81561ecf995e18b12cd6bba44dba59f0c59ea
Edgar González <gonzedge@users.noreply.github.com>
2026-04-05 19:14:22 -0400

    Add rubyzip 3.2 type signature (#1022)
    * init `rubyzip` version `3.2`
    
    * add boilerplate types for `Zip::File` and `Zip::Entry`
    
    from the `rambling-trie` gem:
    https://github.com/gonzedge/rambling-trie/tree/bc3cb892096887bdd2e50594143047a77cfa5b6e/sig/lib/zip
    
    * first pass at generating all .rbs files
    
    * first pass at test_example, based off of 2.3
    
    * add `zlib` dependency
    
    * remove `DelegateClass(Entry)`, use `< Entry` to get around it
    
    unfortunately, `DelegateClass[Entry]` does not work yet per upstream rbs
    
    there are Github issues requesting that feature since Nov 2021,
    last one mentioning it is https://github.com/ruby/rbs/pull/2061
    
    * add `singleton` to deps
    
    * use `extend` for `FileSplit`, not `include`
    
    and fix tests corresponding tests to use correct params!
    
    * fix `(T | void)` to `T?`
    
    * rubocop -A

---
+++ gems/rubyzip/3.2/_test/test_example.rb
@@ -0,0 +1,67 @@
+# Taken from https://github.com/rubyzip/rubyzip/blob/v3.2.0/samples/example.rb
+
+require 'zip'
+
+####### Using ZipInputStream alone: #######
+
+Zip::InputStream.open('example.zip') do |zis|
+  entry = zis.get_next_entry
+  print "First line of '#{entry&.name} (#{entry&.size} bytes):  "
+  puts "'#{zis.gets&.chomp}'"
+  entry = zis.get_next_entry
+  print "First line of '#{entry&.name} (#{entry&.size} bytes):  "
+  puts "'#{zis.gets&.chomp}'"
+end
+
+####### Using ZipFile to read the directory of a zip file: #######
+
+zf = Zip::File.new('example.zip')
+zf.each_with_index do |entry, index|
+  puts "entry #{index} is #{entry.name}, size = #{entry.size}, compressed size = #{entry.compressed_size}"
+  # use zf.get_input_stream(entry) to get a ZipInputStream for the entry
+  # entry can be the ZipEntry object or any object which has a to_s method that
+  # returns the name of the entry.
+end
+
+####### Using ZipOutputStream to write a zip file: #######
+
+Zip::OutputStream.open('exampleout.zip') do |zos|
+  zos.put_next_entry('the first little entry')
+  zos.puts 'Hello hello hello hello hello hello hello hello hello'
+
+  zos.put_next_entry('the second little entry')
+  zos.puts 'Hello again'
+
+  # Use rubyzip or your zip client of choice to verify
+  # the contents of exampleout.zip
+end
+
+####### Using ZipFile to change a zip file: #######
+
+Zip::File.open('exampleout.zip') do |zip_file|
+  zip_file.add('thisFile.rb', 'example.rb')
+  zip_file.rename('thisFile.rb', 'ILikeThisName.rb')
+  zip_file.add('Again', 'example.rb')
+end
+
+# Lets check
+Zip::File.open('exampleout.zip') do |zip_file|
+  puts "Changed zip file contains: #{zip_file.entries.join(', ')}"
+  zip_file.remove('Again')
+  puts "Without 'Again': #{zip_file.entries.join(', ')}"
+end
+
+####### Using ZipFile to split a zip file: #######
+
+# Creating large zip file for splitting
+Zip::OutputStream.open('large_zip_file.zip') do |zos|
+  puts 'Creating zip file...'
+  10.times do |i|
+    zos.put_next_entry("large_entry_#{i}.txt")
+    zos.puts 'Hello' * 104_857_600
+  end
+end
+
+# Splitting created large zip file
+part_zips_count = Zip::File.split('large_zip_file.zip', segment_size: 2_097_152, delete_original: false, partial_zip_file_name: 'part_zip_file')
+puts "Zip file splitted in #{part_zips_count} parts"
