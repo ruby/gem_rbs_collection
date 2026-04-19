bb4866faefceb2b26d335f237c41f1f58409aa88
Hakan Ensari <hakanensari@gmail.com>
2026-04-20 00:44:30 +0200

    Simplify test by removing the Delegate subclass
    Use LLHttp::Delegate.new directly instead of defining a subclass in the
    test. Drops the companion test.rbs since there's no custom Ruby class
    left to declare. Steep already validates the subclass case when it
    loads http.rbs (class Handler < LLHttp::Delegate), so the test doesn't
    need to exercise it too.

--- gems/llhttp-ffi/0.5/_test/test.rb
+++ gems/llhttp-ffi/0.5/_test/test.rb
@@ -1,9 +1,6 @@
 require "llhttp"
 
-class MyDelegate < LLHttp::Delegate
-end
-
-parser = LLHttp::Parser.new(MyDelegate.new, type: :response)
+parser = LLHttp::Parser.new(LLHttp::Delegate.new, type: :response)
 parser.reset
 parser << "HTTP/1.1 200 OK\r\n\r\n"
 parser.status_code
