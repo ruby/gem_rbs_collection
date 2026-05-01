81fa8bd0617286078617a62b6a3229cebfd4af23
Takeshi KOMIYA <i.tkomiya@gmail.com>
2026-05-02 04:54:47 +0900

    thor: Add type for Thor#class_option (#1036)
    ref: https://github.com/rails/thor/blob/v1.2.2/lib/thor/base.rb#L312

--- gems/thor/1.2/_test/test.rb
+++ gems/thor/1.2/_test/test.rb
@@ -4,6 +4,8 @@
 require "thor"
 
 class CLI < Thor
+  class_option :test_framework, :default => :test_unit
+
   desc "usage", "description"
   option :message, type: :string, desc: "description", required: false, aliases: "-m", default: "Hello"
   def example(*args)
