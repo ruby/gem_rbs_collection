1f0394b9cccc963f969d066d9e7d2e42f4f59199
Tëma Bolshakov <tema.bolshakov@workato.com>
2026-07-29 15:05:53 +0300

    Add concurrent ruby thread pool executors (#1054)
    * Add Concurrent::FixedThreadPool signatures
    
    * Add Concurrent::ImmediateExecutor signatures

--- gems/concurrent-ruby/1.1/_test/executor.rb
+++ gems/concurrent-ruby/1.1/_test/executor.rb
@@ -7,3 +7,41 @@ Concurrent.executor(:immediate).post { p :immediate }
 Concurrent.global_io_executor.post { p :io }
 Concurrent.global_fast_executor.post { p :fast }
 Concurrent.global_immediate_executor.post { p :immediate }
+
+pool = Concurrent::FixedThreadPool.new(4, name: "pool", max_queue: 10, idletime: 30, fallback_policy: :abort)
+pool.post { p :fixed_thread_pool }
+pool.fallback_policy #=> :abort
+pool.name #=> "pool"
+pool.max_length #=> 4
+pool.min_length #=> 4
+pool.idletime #=> 30
+pool.max_queue #=> 10
+pool.synchronous #=> false
+pool.largest_length
+pool.scheduled_task_count
+pool.completed_task_count
+pool.length
+pool.queue_length
+pool.remaining_capacity
+pool.can_overflow?
+pool.serialized? #=> false
+pool.running? #=> true
+pool.shuttingdown? #=> false
+pool.shutdown? #=> false
+pool.auto_terminate? #=> true
+pool.to_s
+pool.prune_pool
+pool.shutdown #=> true
+pool.wait_for_termination(1) #=> true
+pool.kill #=> true
+
+immediate = Concurrent::ImmediateExecutor.new
+immediate.post { p :immediate_executor }
+immediate.can_overflow? #=> false
+immediate.serialized? #=> true
+immediate.running? #=> true
+immediate.shuttingdown? #=> false
+immediate.shutdown? #=> false
+immediate.shutdown #=> true
+immediate.wait_for_termination(1) #=> true
+immediate.kill #=> true
